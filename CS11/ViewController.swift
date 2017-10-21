92eb32fe3788ab36df80751279ddb79d3221d1f8
preethiper <preethip@gmail.com>
2017-10-21 16:33:42 -0700

    Lab 2, take 1

--- CS11/ViewController.swift
+++ CS11/ViewController.swift
@@ -19,7 +19,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Properties
+    @IBOutlet weak var textFieldLeft: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textFieldRight: UITextField!
+    
+    //MARK: Actions
+    
+    @IBAction func sendLeft(_ sender: UIButton) {
+        let leftText = textFieldLeft.text!;
+        let newText = label.text! + leftText;
+        label.text = newText;
+    }
+    
+    @IBAction func sendRight(_ sender: UIButton) {
+        let rightText = textFieldRight.text!;
+        let newText = label.text! + rightText;
+        label.text = newText;
+    }
+    @IBAction func clearLabel(_ sender: UIButton) {
+        label.text = "";
+    }
+    
+    /*
+    @IBAction func clearVowels(_ sender: UIButton) {
+        for char in label.text! {
+            if (char == "a" || char == "e" || char == "i"
+                            || char == "o" || char == "u") {
+                let char = "";
+            }
+        }
+    }
+ */
 }
 
